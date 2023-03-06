e607799be65d57fa7ca9668bda2b8d5832058bc7
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-06 14:41:27 -0500

    commit tree cdb804eaa5590883e51f7de09e2eb03cae4eacb2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match(list)
+        result = []
+        list.map do |word|
+            if (word.chars.sort == @word.chars.sort)
+            result << word
+            end
+        end
+        result
+    end
+
+end
